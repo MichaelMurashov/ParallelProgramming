835dd814c405f4a84182dc13ff556102cad0a155
Michael Murashov <mmv047@gmail.com>
2016-11-09 12:15:05 +0300

    fix

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,54 +1,64 @@
 #include <mpi.h>
-#include "semaphore.h"
+#include <iostream>
 
-int readCount = 0;
-Semaphore *readSem, *access;
-
-void writer() {
-  while (true) {
-    access->wait();
-
-    // write
-
-    access->signal();
-  }
-}
-
-void reader() {
-  while (true) {
-    readSem->wait();
-    readCount++;
-
-    if (readCount == 1)
-      access->wait();
-
-    readSem->signal();
-
-    // read
-
-    readSem->wait();
-    readCount--;
-
-    if (readCount == 0)
-      access->signal();
-
-    readSem->wait();
-  }
-}
+using std::cout;
 
 int main(int argc, char** argv) {
+  const int size = 10;
   int procNum, procRank;
-  double start, stop;
+  int flag = true;
   
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &procNum);
   MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
 
+
+  int* data = new int[size];
+  for (int i = 0; i < size; i++)
+    data[i] = i;
+
+
+
   if (procRank == 0) {
-    access = new Semaphore(procNum);
-    readSem = new Semaphore(procNum);
+    cout << "data: ";
+    for (int i = 0; i < size; i++)
+      cout << data[i] << ' ';
+    cout << '\n';
   }
 
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  // writer
+  if (procRank % 2 != 0)
+    while (true) {
+      if (flag == true) {
+        flag = false;
+
+
+        cout << "write " << procRank << ": ";
+        for (int i = 0; i < size; i++) {
+          data[i] += 10;
+          cout << data[i] << ' ';
+        }
+        cout << '\n';
+
+
+
+        flag = true;
+
+        break;
+      }
+    }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  // reader
+  if (procRank % 2 == 0) {
+    cout << "read " << procRank << ": ";
+    for (int i = 0; i < size; i++)
+      cout << data[i] << ' ';
+    cout << "\n";
+  }
 
 
   MPI_Finalize();
