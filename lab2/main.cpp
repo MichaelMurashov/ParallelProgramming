f777c3837de306be2b151837f958a3c8617e989f
Michael Murashov <mmv047@gmail.com>
2016-11-16 19:09:11 +0300

    fix

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -11,7 +11,8 @@ int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &procNum);
   MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-  MPI_Request request;
+  MPI_Request requestFlag, requestData;
+  MPI_Status status;
 
   int* data = new int[size];
   for (int i = 0; i < size; i++)
@@ -29,25 +30,27 @@ int main(int argc, char** argv) {
   // writer
   if (procRank % 2 != 0)
     while (true) {
-      MPI_Irecv(&flag, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &request);
-
       if (flag == true) {
         flag = false;
-        MPI_Isend(&flag, 1, MPI_INT, procRank, 0, MPI_COMM_WORLD, &request);
+        MPI_Isend(&flag, 1, MPI_INT, procRank, 0, MPI_COMM_WORLD, &requestFlag);
 
-        MPI_Irecv(data, size, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &request);
+        MPI_Irecv(data, size, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &requestData);
+        MPI_Wait(&requestData, &status);
         cout << "write " << procRank << ": ";
         for (int i = 0; i < size; i++) {
           data[i] += 10;
           cout << data[i] << ' ';
         }
         cout << '\n';
-        MPI_Isend(data, size, MPI_CHAR, procRank, 1, MPI_COMM_WORLD, &request);
+        MPI_Isend(data, size, MPI_CHAR, procRank, 1, MPI_COMM_WORLD, &requestData);
 
         flag = true;
-        MPI_Isend(&flag, 1, MPI_INT, procRank, 0, MPI_COMM_WORLD, &request);
+        MPI_Isend(&flag, 1, MPI_INT, procRank, 0, MPI_COMM_WORLD, &requestFlag);
         break;
       }
+
+      MPI_Irecv(&flag, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &requestFlag);
+      MPI_Wait(&requestFlag, &status);
     }
 
   MPI_Barrier(MPI_COMM_WORLD);
