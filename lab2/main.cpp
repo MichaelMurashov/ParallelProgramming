21165940e599e6d9d67a5d7c97f24f2bb9be54cc
Michael Murashov <mmv047@gmail.com>
2016-11-19 23:51:39 +0300

    fix

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,44 +3,121 @@
 
 using std::cout;
 
+struct Info {
+  int procRank;
+  int need;
+  int access;
+};
+
+const int WRITE   = 1;
+const int W_DONE  = 3;
+const int STOP    = 5;
+
 int main(int argc, char** argv)
 {
   int procNum, procRank;
-  int flag = 0;
+  Info info;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &procNum);
   MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
   MPI_Status status;
 
-  // выделим в отдельный коммуникатор первую половину процессов
-  /*MPI_Group group_world, writers;
-  MPI_Comm_group(MPI_COMM_WORLD, &group_world);
-  int size = procNum / 2;
-  int* members = new int[size];
-  for (int i = 0; i < size; i++)
-    members[i] = 2 * i;
-  MPI_Group_incl(group_world, size, members, &writers);
-  MPI_Comm writes_comm;
-  MPI_Comm_create(MPI_COMM_WORLD, writers, &writes_comm);*/
+  // manager
+  if (procRank == 0) {
+    int countWriters = 0;
+    int done = 0;
+    int* flags = new int[procNum];
+    for (int i = 0; i < procNum; i++)
+      flags[i] = false;
 
-  MPI_Barrier(MPI_COMM_WORLD);
+    while (true) {
+      // receive msg
+      MPI_Recv(&info, 3, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+
+      if (countWriters == 0)
+        if (flags[info.procRank] == false) {
+          flags[info.procRank] = true;
+          info.access = true;
+        } else {
+          info.access = false;
+        }
+
+      switch (info.need)
+      {
+      case WRITE:
+        countWriters++;
+        break;
+
+      case W_DONE:
+        countWriters--;
+        done++;
+        break;
+
+      default:
+        break;
+      }
+
+      if (done == procNum / 2) {
+        info.access = STOP;
+        cout << "hello ";
+        for (int i = 0; i < procNum; i++)
+          MPI_Send(&info, 3, MPI_INT, i, 0, MPI_COMM_WORLD);
+        break;
+      }
+
+      // send answer
+      MPI_Send(&info, 3, MPI_INT, info.procRank, 0, MPI_COMM_WORLD);
+    }
+  }
 
   // writers
-  if (procRank == 0)
-    cout << "Writer " << procRank << " wr!\n";
+  if (procRank % 2 == 0)
+    while (true) {
+      info.procRank = procRank;
+      info.need = WRITE;
+      // send request
+      MPI_Send(&info, 3, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      // wait answer
+      MPI_Recv(&info, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-  MPI_Barrier(MPI_COMM_WORLD);
+      if (info.access == STOP)
+        break;
 
-  if (procRank == 2)
-    cout << "Writer " << procRank << " wr!\n";
+      if (info.access == true) {
+        // do something cool
+        cout << "Writer " << procRank << " get access!\n";
 
-  MPI_Barrier(MPI_COMM_WORLD);
+        // msg "done"
+        cout << "Writer " << procRank << " done!\n";
+        info.need = W_DONE;
+        MPI_Send(&info, 3, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      }
+    }
 
   // readers
-  if (procRank % 2 != 0)
-    cout << "Reader " << procRank << " read\n";
+  if (procRank % 2 != 0) 
+    while (true) {
+      info.procRank = procRank;
+      // send request
+      MPI_Send(&info, 3, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      // wait answer
+      MPI_Recv(&info, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
+      if (info.access == STOP) {
+        break;
+      }
+
+      if (info.access == true) {
+        // do something cool
+        cout << "Reader " << procRank << " read!\n";
+
+        // msg "done"
+        MPI_Send(&info, 3, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      }
+    }
+
+  MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
   return 0;
 }
